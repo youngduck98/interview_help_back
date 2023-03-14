8eb47231dab1605c043fdf0a80b325c1009f087d
root <root@ip-172-31-36-242.ap-northeast-2.compute.internal>
2023-03-14 14:03:41 +0000

    interest_list

--- flask_server/main.py
+++ flask_server/main.py
@@ -1,33 +1,33 @@
+import sys, os
 from flask import Flask, request, jsonify
+import config
+from database.db_connect import db
+from database import module
+from flask_restx import Resource, Api
 
-app = Flask(__name__)
+app = Flask(__name__) # app assignment
+app.config.from_object(config) # app setting config through config object(related to DB)
+db.init_app(app)
+api = Api(app) # api that make restapi more easier
 
-def search(a = 1, b = 2):
-    return [a, b]
+@api.route('/test/<string:uuid>')
+class test(Resource):
+    def get(self, uuid):
+        return module.user_info_table.query.filter_by(user_uuid=uuid).first().git_nickname
+    def post(self, uuid):
+        data = request.get_json()
+        return jsonify(data)
 
-@app.route('/user_interest_list', methods=['POST']) #test api
-def user_interest_list():
-    param = request.get_json()
-    print(param)
-    return jsonify(param)
-    #return jsonify({"user_uuid": interest_list[0], "list" : interest_list[1:]})
+@app.route('/')
+def main_page_mesg():
+    return "this is capstone main page\n"
 
-@app.route('/today_question', methods=['POST']) #get echo api
-def get_echo_call():
-    today_list = search(request.get_json()['user_uuid'])
-    return today_list
-
-@app.route('/attendance_checking', methods=['POST'])
-def attd_check():
-    answer = False
-    #answer = query
-    return answer
-
-@app.route('/attendance_info', methods=['POST'])
-def attendance_info():
-    answer = False
-    weak_attendance = search(request.get_json()['user_uuid'])
-    return weak_attendance
+@api.route('/interest_list/<string:uuid>')
+class interest_list(Resource):
+    def get(self, uuid):
+         raw_interest_list = User.query.filter_by(user_uuid=uuid).first().interesting_field
+         interest_list = raw_interest_list.split()
+         return interest_list
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host = "0.0.0.0")
--- flask_server/test.py
+++ flask_server/test.py
@@ -1,24 +1,6 @@
-"""
-this file is created to record the practical code
-may be destroyed before submit
-"""
-
 from flask import Flask, request
-from flask_restx import Resource, Api
-
-#if you want to test the new config, then you can use this.
-def create_app(test_config=None):
-    app = Flask(__name__)
-
-    if test_config is None:
-        app.config.from_object(config)
-    else:
-        app.config.update(test_config)
-		
-    db.init_app(app)
 
 app = Flask(__name__) # app assignment
-api = Api(app) # api that make restapi more easier
 
 @app.route('/')
 def main_page_mesg():
@@ -34,48 +16,6 @@ def user_interest_list():
     param = request.get_json()
     print(param)
     return jsonify(param)
-    #return jsonify({"user_uuid": interest_list[0], "list" : interest_list[1:]})
-
-todos = {}
-count = 1
-
-@api.route('/todos')
-class TodoPost(Resource):
-    def post(self):
-        print("post\n   ")
-        global count
-        global todos
-
-        idx = count
-        count += 1
-        todos[idx] = request.json.get('data')
-        print(todos[idx])
-
-        return {
-            'todo_id': idx,
-            'data': todos[idx]
-        }
-
-@api.route('/todos/<int:todo_id>')
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {
-            'todo_id': todo_id,
-            'data': todos[todo_id]
-        }
-
-    def put(self, todo_id):
-        todos[todo_id] = request.json.get('data')
-        return {
-            'todo_id': todo_id,
-            'data': todos[todo_id]
-        }
-    
-    def delete(self, todo_id):
-        del todos[todo_id]
-        return {
-            "delete" : "success"
-        }
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
