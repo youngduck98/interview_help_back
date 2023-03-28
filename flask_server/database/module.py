9182644e7cd565f2a3667abb5c1c2e98dc9c1176
root <root@ip-172-31-36-242.ap-northeast-2.compute.internal>
2023-03-28 15:14:33 +0000

    module_changed

--- flask_server/database/module.py
+++ flask_server/database/module.py
@@ -3,42 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 from .db_connect import db
 from datetime import datetime
 
-class Achievement(db.Model):
-    __tablename__ = 'Achievement'
-
-    achi_uuid = db.Column(db.String(36), primary_key=True)
-    host_uuid = db.Column(db.ForeignKey('User.user_uuid'), nullable=False, index=True)
-    achi_type = db.Column(db.Integer)
-    achi_text = db.Column(db.String(200))
-    date = db.Column(db.BigInteger)
-
-    User = db.relationship('User', primaryjoin='Achievement.host_uuid == User.user_uuid', backref='achievements')
-
-
-
 class Attendance(db.Model):
     __tablename__ = 'Attendance'
 
     att_uuid = db.Column(db.String(36), primary_key=True)
-    user_uuid = db.Column(db.ForeignKey('User.user_uuid'), index=True)
-    att_continuity = db.Column(db.Integer)
-    att_date = db.Column(db.DateTime)
+    user_uuid = db.Column(db.ForeignKey('User.user_uuid'), nullable=False, index=True)
+    att_date = db.Column(db.DateTime, nullable=False)
 
     User = db.relationship('User', primaryjoin='Attendance.user_uuid == User.user_uuid', backref='attendances')
-    
-    def __init__(self, att_uuid, user_uuid, att_continuity, att_date):
-        self.att_uuid = att_uuid
-        self.user_uuid = user_uuid
-        self.att_continuity = att_continuity
-        self.att_date = att_date
+
 
 
 class CommonQue(db.Model):
     __tablename__ = 'CommonQues'
 
     ques_uuid = db.Column(db.String(36), primary_key=True)
-    contents = db.Column(db.String(1000))
-    ques_type = db.Column(db.Integer)
+    question = db.Column(db.String(1000))
+    ques_type = db.Column(db.Integer, server_default=db.FetchedValue())
+    recommandation = db.Column(db.Integer, server_default=db.FetchedValue())
 
 
 
@@ -54,69 +36,62 @@ class IndividualQue(db.Model):
 
 
 
-class InterviewLog(db.Model):
-    __tablename__ = 'InterviewLog'
-
-    log_uuid = db.Column(db.String(36), primary_key=True)
-    inter_ques_uuid = db.Column(db.ForeignKey('MockInterview.interview_uuid'), index=True)
-    log_date = db.Column(db.DateTime)
-    log_content = db.Column(db.String(1000))
-    log_type = db.Column(db.Integer)
-    log_progress = db.Column(db.Integer)
-
-    MockInterview = db.relationship('MockInterview', primaryjoin='InterviewLog.inter_ques_uuid == MockInterview.interview_uuid', backref='interview_logs')
-
-
-
-class InterviewQuestion(db.Model):
-    __tablename__ = 'InterviewQuestion'
-
-    inter_ques_uuid = db.Column(db.String(36), primary_key=True)
-    interview_uuid = db.Column(db.ForeignKey('MockInterview.interview_uuid'), index=True)
-    common_question_uuid = db.Column(db.ForeignKey('CommonQues.ques_uuid'), index=True)
-    indiv_ques_uuid = db.Column(db.String(36))
-    type = db.Column(db.Integer)
-
-    CommonQue = db.relationship('CommonQue', primaryjoin='InterviewQuestion.common_question_uuid == CommonQue.ques_uuid', backref='interview_questions')
-    MockInterview = db.relationship('MockInterview', primaryjoin='InterviewQuestion.interview_uuid == MockInterview.interview_uuid', backref='interview_questions')
-
-
-
-class ItemSelfIntroduction(db.Model):
-    __tablename__ = 'ItemSelfIntroduction'
-
-    script_item_uuid = db.Column(db.String(36), primary_key=True)
-    script_uuid = db.Column(db.ForeignKey('SynthesisSelfIntroduction.script_uuid'), index=True)
-    question = db.Column(db.String(1000))
-    contents = db.Column(db.String(5000))
-    index = db.Column(db.Integer)
-
-    SynthesisSelfIntroduction = db.relationship('SynthesisSelfIntroduction', primaryjoin='ItemSelfIntroduction.script_uuid == SynthesisSelfIntroduction.script_uuid', backref='item_self_introductions')
-
-
-
 class MockInterview(db.Model):
     __tablename__ = 'MockInterview'
 
     interview_uuid = db.Column(db.String(36), primary_key=True)
-    interview_host_uuid = db.Column(db.ForeignKey('User.user_uuid'), index=True)
-    referenced_script = db.Column(db.ForeignKey('SynthesisSelfIntroduction.script_uuid'), index=True)
-    score = db.Column(db.Integer)
+    interview_host_uuid = db.Column(db.ForeignKey('User.user_uuid'), nullable=False, index=True)
+    score = db.Column(db.Integer, server_default=db.FetchedValue())
+    referenced_script = db.Column(db.ForeignKey('SynthesisSelfIntroduction.script_uuid'), nullable=False, index=True)
+    individual_question1 = db.Column(db.ForeignKey('IndividualQues.ques_uuid'), index=True)
+    individual_question2 = db.Column(db.ForeignKey('IndividualQues.ques_uuid'), index=True)
+    common_question1 = db.Column(db.ForeignKey('CommonQues.ques_uuid'), index=True)
+    common_question2 = db.Column(db.ForeignKey('CommonQues.ques_uuid'), index=True)
+    common_question3 = db.Column(db.ForeignKey('CommonQues.ques_uuid'), index=True)
     end_time = db.Column(db.DateTime)
     self_memo = db.Column(db.String(5000))
 
+    CommonQue = db.relationship('CommonQue', primaryjoin='MockInterview.common_question1 == CommonQue.ques_uuid', backref='commonque_commonque_mock_interviews')
+    CommonQue1 = db.relationship('CommonQue', primaryjoin='MockInterview.common_question2 == CommonQue.ques_uuid', backref='commonque_commonque_mock_interviews_0')
+    CommonQue2 = db.relationship('CommonQue', primaryjoin='MockInterview.common_question3 == CommonQue.ques_uuid', backref='commonque_commonque_mock_interviews')
+    IndividualQue = db.relationship('IndividualQue', primaryjoin='MockInterview.individual_question1 == IndividualQue.ques_uuid', backref='individualque_mock_interviews')
+    IndividualQue1 = db.relationship('IndividualQue', primaryjoin='MockInterview.individual_question2 == IndividualQue.ques_uuid', backref='individualque_mock_interviews_0')
     User = db.relationship('User', primaryjoin='MockInterview.interview_host_uuid == User.user_uuid', backref='mock_interviews')
     SynthesisSelfIntroduction = db.relationship('SynthesisSelfIntroduction', primaryjoin='MockInterview.referenced_script == SynthesisSelfIntroduction.script_uuid', backref='mock_interviews')
+
+
+
+class SelfIntroductionA(db.Model):
+    __tablename__ = 'SelfIntroduction_A'
+
+    script_ans_uuid = db.Column(db.String(36), primary_key=True)
+    user_uuid = db.Column(db.ForeignKey('User.user_uuid'), nullable=False, index=True)
+    script_ques_uuid = db.Column(db.ForeignKey('SynthesisSelfIntroduction.script_uuid'), nullable=False, index=True)
+    answer = db.Column(db.String(5000))
+
+    SynthesisSelfIntroduction = db.relationship('SynthesisSelfIntroduction', primaryjoin='SelfIntroductionA.script_ques_uuid == SynthesisSelfIntroduction.script_uuid', backref='self_introduction_as')
+    User = db.relationship('User', primaryjoin='SelfIntroductionA.user_uuid == User.user_uuid', backref='self_introduction_as')
+
+
+
+class SelfIntroductionQ(db.Model):
+    __tablename__ = 'SelfIntroduction_Q'
+
+    script_ques_uuid = db.Column(db.String(36), primary_key=True)
+    question = db.Column(db.String(1000), nullable=False)
+    index = db.Column(db.Integer, nullable=False)
+    tip = db.Column(db.String(1500))
 
 
 
 class SynthesisSelfIntroduction(db.Model):
     __tablename__ = 'SynthesisSelfIntroduction'
 
-    script_uuid = db.Column(db.String(36), primary_key=True)
-    script_host = db.Column(db.ForeignKey('User.user_uuid'), index=True)
-    script_date = db.Column(db.DateTime)
-    script_title = db.Column(db.String(200))
+    script_uuid = db.Column(db.String(45), primary_key=True)
+    script_host = db.Column(db.ForeignKey('User.user_uuid'), nullable=False, index=True)
+    script_date = db.Column(db.String(45), nullable=False)
+    script_title = db.Column(db.String(45), nullable=False)
+    question = db.Column(db.String(200))
 
     User = db.relationship('User', primaryjoin='SynthesisSelfIntroduction.script_host == User.user_uuid', backref='synthesis_self_introductions')
 
@@ -126,8 +101,8 @@ class TodayQue(db.Model):
     __tablename__ = 'TodayQues'
 
     tq_uuid = db.Column(db.String(36), primary_key=True)
-    user_uuid = db.Column(db.ForeignKey('User.user_uuid'), index=True)
-    today_ques = db.Column(db.ForeignKey('CommonQues.ques_uuid'), index=True)
+    user_uuid = db.Column(db.ForeignKey('User.user_uuid'), nullable=False, index=True)
+    today_ques = db.Column(db.ForeignKey('CommonQues.ques_uuid'), nullable=False, index=True)
     user_memo = db.Column(db.String(800))
     date = db.Column(db.DateTime)
 
@@ -145,4 +120,30 @@ class User(db.Model):
     name = db.Column(db.String(100))
     email = db.Column(db.String(200))
     att_continue = db.Column(db.Integer, server_default=db.FetchedValue())
+
+
+
+class CommentRecommandation(db.Model):
+    __tablename__ = 'comment_recommandation'
+
+    cr_uuid = db.Column(db.String(36), primary_key=True)
+    user_uuid = db.Column(db.ForeignKey('User.user_uuid'), nullable=False, index=True)
+
+    User = db.relationship('User', primaryjoin='CommentRecommandation.user_uuid == User.user_uuid', backref='comment_recommandations')
+
+
+
+class CommunityComment(db.Model):
+    __tablename__ = 'community_comment'
+
+    cc_uuid = db.Column(db.String(36), primary_key=True)
+    user_uuid = db.Column(db.ForeignKey('User.user_uuid'), nullable=False, index=True)
+    common_ques = db.Column(db.ForeignKey('CommonQues.ques_uuid'), nullable=False, index=True)
+    comment = db.Column(db.String(5000), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    recommandation = db.Column(db.Integer, server_default=db.FetchedValue())
+
+    CommonQue = db.relationship('CommonQue', primaryjoin='CommunityComment.common_ques == CommonQue.ques_uuid', backref='community_comments')
+    User = db.relationship('User', primaryjoin='CommunityComment.user_uuid == User.user_uuid', backref='community_comments')
+
         
